60736181fa52daed3a3bf2edbc8998b02514b4d4
Miha Čančula <miha@noughmad.eu>
2012-11-04 20:34:30 +0100

    Add license headers to all source files

--- Isle.py
+++ Isle.py
@@ -1,3 +1,22 @@
+"""
+Copyright 2012 Miha Čančula <miha@noughmad.eu>
+This file is part of Isle, a program for displaying transcripts.
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with This program. If not, see http://www.gnu.org/licenses/.
+
+"""
+
 import sys
 from mainwindow import MainWindow
 from PyQt4.QtGui import QApplication
